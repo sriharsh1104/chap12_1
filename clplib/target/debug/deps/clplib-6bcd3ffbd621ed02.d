f96e68699a7332f2cb4343303bea42ad945c95f8
Prashant Sriharsh <prashantsriharsh1104@gmail.com>
2021-12-23 18:21:45 +0530

    command line program

---
+++ clplib/target/debug/deps/clplib-6bcd3ffbd621ed02.d
@@ -0,0 +1,7 @@
+/home/user/prac10/clp2/clplib/target/debug/deps/clplib-6bcd3ffbd621ed02.rmeta: src/lib.rs
+
+/home/user/prac10/clp2/clplib/target/debug/deps/libclplib-6bcd3ffbd621ed02.rlib: src/lib.rs
+
+/home/user/prac10/clp2/clplib/target/debug/deps/clplib-6bcd3ffbd621ed02.d: src/lib.rs
+
+src/lib.rs:
---
+++ clplib/target/debug/libclplib.d
@@ -0,0 +1,1 @@
+/home/user/prac10/clp2/clplib/target/debug/libclplib.rlib: /home/user/prac10/clp2/clplib/src/lib.rs
